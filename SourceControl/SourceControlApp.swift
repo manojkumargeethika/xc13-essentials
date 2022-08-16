4901ac6701b9b9d91fecc55ef69a5f83708d73fe
Geethika Manojkumar <manojkumargeethika@gmail.com>
2022-08-16 13:46:10 -0400

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Geethika Manojkumar on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
